be3b30d82a0d880b799010eb29e899477e22d6bd
DedNikolai <nikolai.blashchuk@gmail.com>
2024-02-06 16:21:08 +0200

    add redux

---
+++ src/utils/api.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const axiosInstance = axios.create({
+    baseURL: "http://localhost:8000",
+  });
+
+  axiosInstance.interceptors.request.use(
+    (config) => {
+      const authToken = window.localStorage.getItem("auth-token");
+  
+      if (authToken) {
+        config.headers.authorization = `Bearer ${authToken}`;
+        config.headers['Content-Type'] = 'application/json'
+      }
+  
+      return config;
+    },
+    (error) => Promise.reject(error)
+  );
+  
+  export default axiosInstance;
