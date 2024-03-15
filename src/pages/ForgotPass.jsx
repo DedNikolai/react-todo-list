57fdd3f553186b9631b5e8274c58d32eee0d006d
unknown <nikolai.blashchuk@gmail.com>
2024-03-15 13:16:49 +0200

    forgot pass

--- src/pages/ForgotPass.jsx
+++ src/pages/ForgotPass.jsx
@@ -1,40 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
-import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import {Link, Navigate} from 'react-router-dom';
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
-import {useSelector, useDispatch} from "react-redux";
-import {login} from '../store/slice/user';
+import {useDispatch} from "react-redux";
+import {forgotPass} from '../store/slice/user';
+import Loader from '../components/Loader';
 
 const schema = yup.object({
   email: yup.string().email('Invalid Email').required('Please input email'),
-  password: yup.string().required(),
 }).required();
 
 const defaultTheme = createTheme();
 
-export default function Login() {  
-  const {user} = useSelector(state => state.user);
+export default function ForgotPass() {  
   const dispatch = useDispatch();
+  const [pending, setPending] = useState(false)
   const {register, handleSubmit, formState: {errors}, reset} = useForm({
     resolver: yupResolver(schema),
     mode: 'onBlur'
   });
 
   const onSubmit = (data) => {
-    dispatch(login(data));
+    setPending(true)
+    dispatch(forgotPass(data)).finally(() => {
+        console.log('ok')
+        setPending(false)
+    });
     reset();
   };
 
-  if (user) return <Navigate to='/' />
+  if (pending) return <Loader />
 
   return (
     <div className="container">
@@ -51,7 +53,7 @@ export default function Login() {
                 <LockOutlinedIcon />
               </Avatar>
               <Typography component="h1" variant="h5">
-                Sign in
+                Forgot Password
               </Typography>
               <Box component="form" onSubmit={handleSubmit(onSubmit)} noValidate sx={{ mt: 1 }}>
                 <TextField
@@ -66,38 +68,14 @@ export default function Login() {
                   autoComplete="email"
                   autoFocus
                 />
-                <TextField
-                  {...register("password")}
-                  label={errors.password?.message || "Input password"}
-                  error={errors.hasOwnProperty('password')}
-                  margin="normal"
-                  required
-                  fullWidth
-                  name="password"
-                  type="password"
-                  id="password"
-                  autoComplete="current-password"
-                />
                 <Button
                   type="submit"
                   fullWidth
                   variant="contained"
                   sx={{ mt: 3, mb: 2 }}
                 >
-                  Sign In
+                  Send
                 </Button>
-                <Grid container>
-                  <Grid item xs>
-                    <Link to="/forgot-pass" className="custom-link">
-                      Forgot password?
-                    </Link>
-                  </Grid>
-                  <Grid item>
-                    <Link to="/register" className="custom-link">
-                      {"Don't have an account? Sign Up"}
-                    </Link>
-                  </Grid>
-                </Grid>
               </Box>
             </Box>
         </ThemeProvider>
