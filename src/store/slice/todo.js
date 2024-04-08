f77e18866a2b0b8f665357eec8ea161711c784ce
unknown <nikolai.blashchuk@gmail.com>
2024-04-08 15:26:42 +0300

    change page size

--- src/store/slice/todo.js
+++ src/store/slice/todo.js
@@ -74,7 +74,7 @@ export const remove = createAsyncThunk('todo/remove', async (id, {rejectWithValu
 });
 
 export const getAll = createAsyncThunk('todo/getAll', async (options, {rejectWithValue}) => {
-    const {isDone, todoDate, page = 1, size = 2} = options
+    const {isDone, todoDate, page = 1, size = 5} = options
 
     try {
         const response = await api.get(`/todos?isDone=${isDone}&todoDate=${todoDate}&page=${page}&size=${size}`);
